47c9c0374784c08f8c3ca83552e8c5c831cba945
mandihamza <mandihamza@gmail.com>
2019-09-16 16:14:13 -0600

    Adds styles for character cards

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,24 @@
-import React from "react";
+import React from 'react'
+import { Card, Icon, Image } from 'semantic-ui-react'
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard({character}) {
+  return (
+      <Card>
+        <Image src={character.image} wrapped ui={false} />
+        <Card.Content>
+          <Card.Header>{character.name}</Card.Header>
+          <Card.Meta>{character.species}</Card.Meta>
+          <Card.Description>
+            Location: {character.location.name}
+            Origin: {character.origin.name}
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <a>
+            <Icon name="user" />
+            {character.episode.length}
+          </a>
+        </Card.Content>
+      </Card>
+  )
 }
