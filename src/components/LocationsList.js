963ac0f9cef996b262ec7d12c6ca863cc668e331
mandihamza <mandihamza@gmail.com>
2019-09-17 15:39:53 -0600

    Completes API calls for LocationCard.js

--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,3 +1,33 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
-export default function LocationsList() {}
+import LocationCard from './LocationCard'
+
+export default function LocationsList() {
+  const [locations, setLocations] = useState([])
+
+  useEffect(() => {
+    axios('https://rickandmortyapi.com/api/location/')
+      .then(res => setLocations(res.data.results))
+      .catch(err => {
+        console.log('err', err)
+      })
+  }, [])
+
+  return (
+    <section className="location-list grid-view">
+      {locations.map(location => {
+        return (
+          <LocationCard
+            location={location}
+            key={location.id}
+            name={location.name}
+            type={location.type}
+            dimension={location.dimension}
+            residents={location.residents}
+          />
+        )
+      })}
+    </section>
+  )
+}
