fb316c69c205cca1cc54f42394a916ca00a15409
mandihamza <mandihamza@gmail.com>
2019-11-05 16:02:38 -0700

    Adds location card stucture

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,5 +1,28 @@
-import React from "react";
+import React from 'react'
+import { Card, List, Label, Icon } from 'semantic-ui-react'
 
-export default function LocationCard({ name, type, dimension, residents }) {
-  return <span>todo: location</span>;
+export default function LocationCard({ location }) {
+  return (
+    <Card>
+      <Card.Header>
+        <Icon fitted name="globe" size="big"/>
+          {location.name}</Card.Header>
+      <Card.Content>
+        <List>
+          <List.Item>
+            <List.Icon name="compass outline" />
+            <List.Content>Type: {location.type}</List.Content>
+          </List.Item>
+          <List.Item>
+            <List.Icon name="move" />
+            <List.Content>Dimension: {location.dimension}</List.Content>
+          </List.Item>
+        </List>
+        <Label attached="bottom right">
+          <Icon name="group" />
+          <Label.Detail>Population: {location.residents.length}</Label.Detail>
+        </Label>
+      </Card.Content>
+    </Card>
+  )
 }
